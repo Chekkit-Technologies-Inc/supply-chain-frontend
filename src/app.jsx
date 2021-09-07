4171851f42fc20817d4b970a4a4e658048f47e47
Michael Nwankwo <mikhaelnwankwo@gmail.com>
2021-09-07 21:03:10 +0100

    updates

--- src/app.jsx
+++ src/app.jsx
@@ -4,7 +4,8 @@ function App() {
   return (
     <div className='min-h-screen max-h-screen w-screen flex relative'>
       <img src={bg} className='w-screen h-screen object-cover object-left' alt='logo' />
-      <div className={`bg-brand_blue absolute right-0 top-0 bottom-0 z-10 w-full max-w-3xl h-screen`}></div>
+
+      <div style={{ minWidth: '48rem' }} className={`bg-brand_blue absolute right-0 top-0 bottom-0 z-10 w-5/12 h-screen`}></div>
     </div>
   );
 }

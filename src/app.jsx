2145f5555a7d4cfe889717c0870724aca48702d9
Michael Nwankwo <mikhaelnwankwo@gmail.com>
2021-09-07 20:05:32 +0100

    updates

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,10 @@
-import bg from './assets/asset-mgmt.jpeg';
+import bg from './assets/asset-m.png';
 
 function App() {
   return (
     <div className='min-h-screen max-h-screen w-screen flex relative'>
       <img src={bg} className='w-screen h-screen object-cover object-left' alt='logo' />
-      <div style={{ minWidth: '400px' }} className={`bg-brand_blue absolute right-0 top-0 bottom-0 z-10 w-2/5 h-screen`}></div>
+      <div className={`bg-brand_blue absolute right-0 top-0 bottom-0 z-10 w-full max-w-3xl h-screen`}></div>
     </div>
   );
 }

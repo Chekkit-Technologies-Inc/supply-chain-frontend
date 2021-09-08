1d55595090b9724a218e9bdf5d2cbad577a3dff1
Michael Nwankwo <mikhaelnwankwo@gmail.com>
2021-09-08 14:49:21 +0100

    updates

--- src/components/fragments/Button/Button.jsx
+++ src/components/fragments/Button/Button.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React from 'react';
 
-const Button = () => {
-  return <div />;
+const Button = ({ text, type, variant, size, className, icon, placement, onClick }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`${className} ${
+        type === 'secondary'
+          ? `bg-transparent text-brand_blue hover:text-white`
+          : variant !== 1
+          ? `bg-brand_blue text-white border-brand_blue hover:bg-brand_blue`
+          : `bg-brand_green border-brand_green text-brand_blue hover:bg-brand_green`
+      } transition_all border-2 ${size && size === 2 ? `px-4 py-2 text-sm` : ` px-6 py-3 text-lg`} rounded-lg flex justify-center items-center`}
+    >
+      {icon && placement === 'left' && icon}
+      <span className={`px-2 font-semibold`}>{text}</span>
+      {icon && placement === 'right' && icon}
+    </button>
+  );
 };
 
 export default Button;
--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-import bg from '../../assets/asset-mm.png';
+import bg from '../../assets/asset-m.png';
 
 import { Tabs, Panel } from '../../components/fragments/Tabs';
+import Login from '../../components/fragments/Login';
+import SignUp from '../../components/fragments/SignUp';
 
 const Auth = () => {
   return (
@@ -12,8 +14,12 @@ const Auth = () => {
       <div className={`bg-brand_blue absolute right-0 top-0 bottom-0 z-10 w-full lg:w-5/12 h-screen flex justify-center items-center`}>
         <div className={`w-full max-w-md m-12 p-12 border border-brand_blue_light rounded-2xl`}>
           <Tabs>
-            <Panel title={'Log In'}>Hello 1</Panel>
-            <Panel title={'Sign Up'}>Hello 2</Panel>
+            <Panel title={'Log In'}>
+              <Login />
+            </Panel>
+            <Panel title={'Sign Up'}>
+              <SignUp />
+            </Panel>
           </Tabs>
         </div>
       </div>

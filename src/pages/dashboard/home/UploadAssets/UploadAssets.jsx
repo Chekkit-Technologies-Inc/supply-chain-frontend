33e44d518fe3a625f921f141f18aec1e51c91667
Michael Nwankwo <mikhaelnwankwo@gmail.com>
2021-09-23 12:43:40 +0100

    updates

--- src/pages/dashboard/home/UploadAssets/UploadAssets.jsx
+++ src/pages/dashboard/home/UploadAssets/UploadAssets.jsx
@@ -8,18 +8,20 @@ import Button from '../../../../components/fragments/Button'
 
 
 const UploadAssets = () => {
-  return <div className={`space-y-6 py-16 px-4 bg-white h-full`}>
+  return <div className={`space-y-6 py-16 px-4 bg-white `}>
     <Heading className={`font-medium text-brand_blue`} title={`Select type of asset you want to onboard`} size={2} />
-    <AssetUploadList />
-    <div className={`flex justify-center`} >
-      <DragDropZone>
-        <Text className={`text-lg font-medium text-brand_blue underline`} value={`Select CSV file`} />
-        <Text className={`text-sm text-gray-400`} value={`or drop your file here`} />
-      </DragDropZone>
-    </div>
-    <div className={`flex space-x-4 md:space-x-12 justify-center`}>
-        <Button text={`Back`} type={`secondary`} />
-        <Button text={`Continue`} />
+    <div className={`space-y-12`}>
+      <AssetUploadList />
+      <div className={`flex justify-center`} >
+        <DragDropZone>
+          <Text className={`text-lg font-medium text-brand_blue underline`} value={`Select CSV file`} />
+          <Text className={`text-sm text-gray-400`} value={`or drop your file here`} />
+        </DragDropZone>
+      </div>
+      <div className={`flex space-x-4 md:space-x-12 justify-center`}>
+          <Button text={`Back`} type={`secondary`} />
+          <Button text={`Continue`} />
+      </div>
     </div>
   </div>;
 };

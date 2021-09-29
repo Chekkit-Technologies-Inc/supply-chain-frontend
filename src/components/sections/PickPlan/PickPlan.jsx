b3f2ec5bfd5c0143b604dc279617fdf5c195e310
Michael Nwankwo <mikhaelnwankwo@gmail.com>
2021-09-29 18:26:12 +0100

    updates

--- src/components/sections/PickPlan/PickPlan.jsx
+++ src/components/sections/PickPlan/PickPlan.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 
 import PlanList from '../../fragments/PlanList';
+
+
 import { setupData as data } from '../../../appData';
 
 const PickPlan = ({ onComplete }) => {
   return (
-    <div className={`flex flex-col justify-center items-center space-y-6 p-6 pb-16 w-full min-h-screen mx-auto`}>
-      <div>Emmanuel</div>
+    <div className={`flex flex-col justify-between items-center space-y-6 p-6 py-16 w-full min-h-screen mx-auto`}>
+      <div className={`text-center text-xl space-y-1`}>
+        <div>Thank you for signing up <span className={`font-bold text-brand_blue`}>Emmanuel,</span></div>
+        <div>Please select a plan so we can tailor your experience</div>
+      </div>
       <PlanList items={data.pickPlan} onComplete={onComplete} />
-      <div>Schedule</div>
+      <div className={`text-center`}>
+        <div className={`text-brand_blue_light`}>Would you like to talk to us more about this? <span className={`font-bold text-brand_blue`}>Schedule a Call</span></div>
+      </div>
     </div>
   );
 };

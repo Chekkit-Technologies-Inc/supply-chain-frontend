9648deb1c9b7a9e1b42885b449cd38882101ba03
Michael Nwankwo <mikhaelnwankwo@gmail.com>
2021-10-04 09:57:18 +0100

    updates

--- src/components/fragments/OptionList/OptionList.jsx
+++ src/components/fragments/OptionList/OptionList.jsx
@@ -1,31 +1,76 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { MdDelete } from 'react-icons/md';
+
 import { ReactComponent as DragIcon } from '../../../assets/drag-item.svg';
 import { ReactComponent as Checked } from '../../../assets/radio-checked.svg';
 import { ReactComponent as Unchecked } from '../../../assets/radio-unchecked.svg';
 
 import InputBox from '../InputBox/InputBox';
 
-const OptionList = () => {
+const OptionList = ({data}) => {
+  const [options, setOptions] = useState([])
+
+  useEffect(() => {
+    if (data) {
+      setOptions(data);
+    }
+  }, [data]);
+
+  const addOption = () => {
+    if (options.length > 3) return;
+    setOptions([...options, {value: '', selected: false}])
+  }
+
+  const deleteOption = (idx) => {
+    const optionsList = options.filter((d, i) => {
+      return i !== idx
+    })
+    setOptions(optionsList)
+  }
+
   return (
     <div className={`w-full max-w-md flex flex-col items-center space-y-6`}>
-      <div className={`w-full text-gray-500`}>options</div>
+      <div className={`w-full text-gray-500`}>Options</div>
       <ul className={`w-full space-y-6`}>
-        <li className={`flex items-center space-x-4`}>
-          <DragIcon className={`cursor-move`} />
-          <div className={`cursor-pointer`}>{'allSelected' ? <Checked /> : <Unchecked />}</div>
-          <InputBox placeholder={`Type answer here...`} variant={3} />
-        </li>
-        <li className={`flex items-center space-x-4`}>
-          <DragIcon className={`cursor-move`} />
-          <div className={`cursor-pointer`}>{'' ? <Checked /> : <Unchecked />}</div>
-          <InputBox placeholder={`Type answer here...`} variant={3} />
-        </li>
+        {options.length > 0 && options.map((option, idx) => {
+          return (
+            <Item key={idx} item={option} idx={idx} onDelete={deleteOption} />
+          )
+        })}
       </ul>
       <div className={`flex items-center justify-end w-full`}>
-        <div className={`text-brand_blue font-medium text-sm bg-blue-200 rounded-md p-2 cursor-pointer`}>+ Add Option</div>
+        <div aria-disabled={options.length > 3} onClick={addOption} className={`text-brand_blue font-medium text-sm ${options.length > 3 ? `bg-gray-200 cursor-default` : `bg-blue-200 cursor-pointer`} rounded-md p-2`}>+ Add Option</div>
       </div>
     </div>
   );
 };
+
+const Item = ({item, idx, onDelete}) => {
+  const [option, setOption] = useState();
+
+  useEffect(() => {
+    if (item) {
+      setOption(item);
+    }
+  }, [item]);
+
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+    setOption({ ...option, [name]: value });
+  };
+
+  const toggleSelect = () => {
+    setOption({ ...option, selected: !option.selected });
+  };
+
+  return (
+    <li className={`flex items-center space-x-4`}>
+      <DragIcon className={`cursor-move`} />
+      <div onClick={toggleSelect} className={`cursor-pointer`}>{option?.selected ? <Checked /> : <Unchecked />}</div>
+      <InputBox placeholder={`Type answer here...`} variant={3} value={option?.value} name={`value`} onValueChange={handleInputChange} />
+      <MdDelete onClick={() => onDelete(idx)} className={`text-gray-300 hover:text-gray-600 cursor-pointer`} size={24} />
+    </li>
+  )
+}
 
 export default OptionList;
--- src/components/fragments/QuestionList/QuestionList.jsx
+++ src/components/fragments/QuestionList/QuestionList.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { MdDelete } from 'react-icons/md';
 
 import { ReactComponent as DragIcon } from '../../../assets/drag-item.svg';
 
@@ -6,15 +7,48 @@ import InputBox from '../InputBox/InputBox';
 import SelectBox from '../SelectBox';
 import OptionList from '../OptionList';
 
+const questionData = [
+  {
+    question: '',
+    options: [
+      {
+        value: '',
+        selected: false
+      },
+      {
+        value: '',
+        selected: false
+      }
+    ]
+  }
+]
+
 const QuestionList = () => {
+  const [questions, setQuestions] = useState(questionData)
+
+  const addQuestion = () => {
+    setQuestions([...questions, ...questionData])
+  }
+
+  const deleteQuestion = (idx) => {
+    const questionsList = questions.filter((d, i) => {
+      return i !== idx
+    })
+    setQuestions(questionsList)
+  }
+
   return (
     <div className={`w-full flex flex-col justify-center items-center `}>
-      <DragIcon className={`transform rotate-90 mb-6 cursor-move`} />
-      <InputBox className={`max-w-md mb-12`} label={`Question`} labelColor={`text-gray-500`} placeholder={`Type question here...`} variant={3} />
-      <OptionList />
+      <ul className={`w-full space-y-6`}>
+        {questions.length > 0 && questions.map((question, idx) => {
+          return (
+            <Item key={idx} item={question} idx={idx} onDelete={deleteQuestion} />
+          )
+        })}
+      </ul>
       <SelectBox className={`max-w-md mb-6 mt-10`} placeholder={`Select Asset Batch`} options={['Cat01', 'Cat02']} variant={3} />
       <div className={`w-full max-w-md`}>
-        <div className={`inline-flex items-center space-x-2 cursor-pointer`}>
+        <div onClick={addQuestion} className={`inline-flex items-center space-x-2 cursor-pointer`}>
           <span style={{ paddingTop: '2px' }} className={`w-4 h-4 text-white bg-brand_blue flex justify-center items-center rounded-full`}>
             +
           </span>
@@ -24,5 +58,31 @@ const QuestionList = () => {
     </div>
   );
 };
+
+const Item = ({item, idx, onDelete}) => {
+  const [question, setQuestion] = useState();
+
+  useEffect(() => {
+    if (item) {
+      setQuestion(item);
+    }
+  }, [item]);
+
+  const handleInputChange = event => {
+    const { name, value } = event.target;
+    setQuestion({ ...question, [name]: value });
+  };
+
+  return (
+    <li className={`flex flex-col justify-center items-center border-b border-gray-200 pb-8`}>
+      <DragIcon className={`transform rotate-90 mb-6 cursor-move`} />
+      <div className={`max-w-md w-full mb-12 relative`}>
+        <InputBox label={`Question ${idx + 1}`} name={`question`} value={item?.question} onValueChange={handleInputChange} labelColor={`text-gray-500`} placeholder={`Type question here...`} variant={3} />
+        {idx > 0 && <MdDelete onClick={() => onDelete(idx)} className={`text-gray-300 hover:text-gray-600 cursor-pointer absolute top-0 right-0`} size={20} />}
+      </div>
+      <OptionList data={item.options} />
+    </li>
+  )
+}
 
 export default QuestionList;

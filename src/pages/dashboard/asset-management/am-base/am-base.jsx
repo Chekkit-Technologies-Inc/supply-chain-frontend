eb3fbe980e06888de530dbeaa1c61b39284650cc
Michael Nwankwo <mikhaelnwankwo@gmail.com>
2021-12-09 17:52:13 +0100

    updates

--- src/pages/dashboard/asset-management/am-base/am-base.jsx
+++ src/pages/dashboard/asset-management/am-base/am-base.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import FadeIn from 'react-fade-in/lib/FadeIn';
+import { useSelector } from 'react-redux';
 
 const links = [
-  { name: 'Asset Management & Tracking', url: '/asset-management/#' },
+  { name: 'Asset Management & Tracking', url: 'http://localhost:3001/#/setup/pick-plan' },
   { name: 'Inventory Management', url: '/asset-management/#' },
 ];
 
 const AMBase = () => {
+  const user = useSelector(state => state.user);
   return (
     <FadeIn
       className={`flex flex-col justify-start items-start
@@ -17,13 +18,14 @@ const AMBase = () => {
       <FadeIn className={`max-w-7xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12`}>
         {links.map((link, idx) => {
           return (
-            <Link
+            <a
               key={idx}
-              to={link.url}
+              href={`${link.url}/${user?.token}`}
+              target={`_blank`}
               className={`w-full h-64 rounded-2xl bg-white shadow flex items-center justify-center border cursor-pointer font-semibold hover:shadow-lg p-6 text-center`}
             >
               {link.name}
-            </Link>
+            </a>
           );
         })}
       </FadeIn>

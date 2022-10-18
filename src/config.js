4d4963c721d6c0569306340f64797cd655d5fce1
Michael Nwankwo <mikhaelnwankwo@gmail.com>
2022-10-18 08:22:55 +0100

    updates

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const apiUrl = ' https://api-supply-chain.chekkit.app/api/v1';
-export const apiUrl = 'https://supply-chain-backend.azurewebsites.net/api/v1';
+export const apiUrl = ' https://api-supply-chain.chekkit.app/api/v1';
+// export const apiUrl = 'https://supply-chain-backend.azurewebsites.net/api/v1';

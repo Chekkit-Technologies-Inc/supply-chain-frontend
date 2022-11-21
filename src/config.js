7f1e983d524e83f7a59975c8b3047734113e95dc
Michael Nwankwo <mikhaelnwankwo@gmail.com>
2022-11-21 16:12:27 +0100

    updates for chektrack

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
-// export const apiUrl = 'https://api-supply-chain.chekkit.app/api/v1';
+export const apiUrl = 'https://api-supply-chain.chekkit.app/api/v1';
 
-export const apiUrl = 'https://supply-chain-backend.azurewebsites.net/api/v1';
+// export const apiUrl = 'https://supply-chain-backend.azurewebsites.net/api/v1';
 
 export const REACT_APP_MAP_API_KEY = 'AIzaSyBIhKt7YhWM0qAcLZrIAJGmrqnJLs8dT8E';
 
-// export const REACT_APP_ASSET_MANAGEMNET_URL = 'https://asset-management.chekkit.app/#';
+export const REACT_APP_ASSET_MANAGEMNET_URL = 'https://asset-management.chekkit.app/#';
 
 // export const REACT_APP_ASSET_MANAGEMNET_URL = 'http://localhost:3002/#';
 
-export const REACT_APP_ASSET_MANAGEMNET_URL = 'https://happy-pond-0ca78e203.2.azurestaticapps.net/#';
+// export const REACT_APP_ASSET_MANAGEMNET_URL = 'https://happy-pond-0ca78e203.2.azurestaticapps.net/#';
 
 export const REACT_APP_CONNECT_STRING =
   'InstrumentationKey=01667578-0def-4cdd-a366-d9b748aef3ae;IngestionEndpoint=https://westeurope-5.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/';
